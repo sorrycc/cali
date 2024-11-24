3c56c175d15f74cf5dca19f1f9cdb55e13847598
Mike <grabbou@gmail.com>
2024-11-24 17:07:52 +0100

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
-  
-import { openai } from '@ai-sdk/openai'
+
+import { createOpenAI } from '@ai-sdk/openai'
 import { confirm, log, select, spinner, text } from '@clack/prompts'
 import { CoreMessage, generateText } from 'ai'
 import chalk from 'chalk'
@@ -9,9 +9,9 @@ import { retro } from 'gradient-string'
 import { z } from 'zod'
 
 import { reactNativePrompt } from './prompt'
-import { androidTools } from './tools/android'
-import { iosTools } from './tools/apple'
-import { reactNativeTools } from './tools/react-native'
+import * as androidTools from './tools/android'
+import * as iosTools from './tools/apple'
+import * as reactNativeTools from './tools/react-native'
 
 const MessageSchema = z.union([
   z.object({ type: z.literal('select'), content: z.string(), options: z.array(z.string()) }),
@@ -43,6 +43,39 @@ console.log(
 
 console.log()
 
+const OPENAI_API_KEY =
+  process.env.OPENAI_API_KEY ||
+  ((await text({
+    message: dedent`
+      Please provide your OpenAI API key. 
+      
+      To skip this message, set ${chalk.bold('OPENAI_API_KEY')} env variable, and run again. 
+      
+      You can do it in three ways:
+      - by creating an ${chalk.bold('.env.local')} file (make sure to ${chalk.bold('.gitignore')} it)
+        ${chalk.gray(`\`\`\`
+          OPENAI_API_KEY=<your-key>
+          \`\`\`
+        `)}
+      - by passing it inline:
+        ${chalk.gray(`\`\`\`
+          OPENAI_API_KEY=<your-key> npx cali
+          \`\`\`
+        `)}
+      - by setting it as an env variable in your shell (e.g. in ~/.zshrc or ~/.bashrc):
+        ${chalk.gray(`\`\`\`
+          export OPENAI_API_KEY=<your-key>
+          \`\`\`
+        `)}
+    `,
+  })) as string)
+
+const AI_MODEL = process.env.AI_MODEL || 'gpt-4o'
+
+const openai = createOpenAI({
+  apiKey: OPENAI_API_KEY,
+})
+
 const question = (await text({
   message: 'What do you want to do today?',
 })) as string
@@ -65,7 +98,7 @@ while (true) {
   s.start(chalk.gray('Thinking...'))
 
   const response = await generateText({
-    model: openai('gpt-4o'),
+    model: openai(AI_MODEL),
     system: reactNativePrompt,
     tools: {
       ...reactNativeTools,
